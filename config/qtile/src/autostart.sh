978971865c83e9b4ad1e9244942553e63a1ccef2
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-12-12 09:25:59 -0300

    Update autostart.sh

--- config/qtile/src/autostart.sh
+++ config/qtile/src/autostart.sh
@@ -7,3 +7,8 @@ numlockx &
 if ! pgrep -x "polkit-gnome-au" > /dev/null; then
     /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 fi
+
+# ===== WALLPAPER ALEATÓRIO COM FEH =====
+WALLPAPER_DIR="$HOME/.config/qtile/walls"   # ou essa se preferir
+
+feh --randomize --bg-fill "$WALLPAPER_DIR" &
