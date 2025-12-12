2704259016445f2993933bb46716951a94cd3d02
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-12-11 23:14:45 -0300

    Update autostart.sh

--- config/qtile/src/autostart.sh
+++ config/qtile/src/autostart.sh
@@ -3,3 +3,7 @@ nitrogen --restore &
 #picom &
 vmware-user suid wrapper &
 numlockx &
+
+if ! pgrep -x "polkit-gnome-au" > /dev/null; then
+    /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+fi
