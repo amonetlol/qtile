52c24ae9fdc340d00d8a2d2110adc4623c5bdc76
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-01-05 00:05:27 -0300

    Update autostart.sh

--- config/qtile/src/autostart.sh
+++ config/qtile/src/autostart.sh
@@ -4,14 +4,15 @@
 vmware-user suid wrapper &
 numlockx &
 
+# Gnome-polkit
 if ! pgrep -x "polkit-gnome-au" > /dev/null; then
     /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 fi
 
 # Mate-polkit
-# if ! pgrep -u "$USER" -x mate-polkit > /dev/null; then
-#     /usr/lib/mate-polkit/mate-polkit &
-# fi
+if ! pgrep -u "$USER" -x mate-polkit > /dev/null; then
+    /usr/lib/mate-polkit/mate-polkit &
+fi
 
 # ===== WALLPAPER ALEATÓRIO COM FEH =====
 #setfeh.sh &
