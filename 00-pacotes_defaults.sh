fa9a1889b81140cc9a16d3fdc3ee125d0713e71d
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-02-04 21:23:52 -0300

    Update and rename pacotes_defaults.sh to 00-pacotes_defaults.sh

---
+++ 00-pacotes_defaults.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+bash <(curl -fsSL https://raw.githubusercontent.com/amonetlol/scripts/main/00-pacotes_defaults.sh)
--- pacotes_defaults.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-bash <(curl -fsSL https://raw.githubusercontent.com/amonetlol/scripts/main/pacotes_defaults.sh)
